d4593f16c306c4cf01c25b6ecdf5eef47dcc4e5d
Geoff Elder <gaelder@edu.uwaterloo.ca>
2020-01-29 11:32:41 -0500

    default integration

--- Integrate100kcell.R
+++ Integrate100kcell.R
@@ -4,11 +4,28 @@ seurfiles <- c("C37_SeurObj.rds", "C46_SeurObj.rds", "C52_SeurObj.rds", "C59_Seu
 
 samp_names <- unlist(lapply(strsplit(seurfiles, "_"), function(x){x[[1]]}))
 obj_list <- list()
+common_genes <- c();
 for (i in 1:length(seurfiles)) {
-	n <- samp_names[i]
-	obj <- readRDS(seurfiles[i])
-	obj$meta.data$donor <- rep(n, ncol(obj));
-	obj_list[[n]] <- readRDS(obj)
+	n <- samp_names[i];
+	obj <- readRDS(seurfiles[i]);
+	if (length(common_genes) == 0) {
+		common_genes <- rownames(obj);
+	} else {
+		common_genes <- intersect(common_genes, rownames(obj));
+	}
+	obj@meta.data$donor <- rep(n, ncol(obj));
+	obj_list[[n]] <- obj
+}
+
+common_genes <- sort(common_genes);
+common_genes <- common_genes[!grepl("^MT-", common_genes)]
+for (i in 1:length(obj_list)) {
+	obj_list[[i]] <- obj_list[[i]][match(common_genes, rownames(obj_list[[i]])),]
 }
 
 
+anchors <- FindIntegrationAnchors(object.list=obj_list, dims=1:20)
+
+liver.integrated <- IntegrateData(achorset = anchors, dims=1:20)
+
+saveRDS(liver.integrated, "basicintegration.rds")
