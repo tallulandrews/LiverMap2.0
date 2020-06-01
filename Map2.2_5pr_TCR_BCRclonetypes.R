7289859a6aa102b35f6771181c1f30bc06e54fe7
tallulandrews <tallulandrews@gmail.com>
2020-06-01 10:19:31 -0400

    initial processing of TCR/BCR data.

--- Map2.2_5pr_TCR_BCRclonetypes.R
+++ Map2.2_5pr_TCR_BCRclonetypes.R
@@ -1,138 +1,63 @@
 require("Seurat")
-source("My_R_Scripts.R")
+source("/cluster/home/tandrews/scripts/LiverMap2.0/My_R_Scripts.R")
+require("DropletUtils")
 
-proj_name = "Tcell"
+loc_file <- read.delim("/cluster/home/tandrews/scripts/LiverMap2.0/TCR_BCR_clonetype_files.csv", sep=",", header=T, stringsAsFactors=FALSE)
 
-liver.integrated <- readRDS("integration_harmony_plus_analysis.rds")
+MAX_CELLS=25000
+MIN_CELLS=100
 
-cluster_labs <- liver.integrated@meta.data$Fine_clusters
-names(cluster_labs) <- paste(liver.integrated@meta.data$donor, liver.integrated@meta.data$cell_barcode, sep="_")
-harmony_redim <- liver.integrated@reductions$harmony
-meta.data <- liver.integrated@meta.data
-meta.data <- meta.data[,!grepl("^knn", colnames(meta.data))]
+i = 1;
 
+name=loc_file[i,1]
+# Read and TCRs & BCRs
+tcrs <- read.delim(loc_file[i,"TCR_file"], sep=",", header=T)
+bcrs <- read.delim(loc_file[i,"BCR_file"], sep=",", header=T)
 
-rm(liver.integrated)
-liver.integrated <- readRDS("all_gene_merged_obj.rds")
+# Read raw mat and tidy
+filt <- Read10X(loc_file[i,"X5pr_filtdir"])
+raw_mat <- Read10X(loc_file[i,"X5pr_rawdir"])
+raw_mat <- raw_mat[,Matrix::colSums(raw_mat) > 0]
+raw_mat <- raw_mat[Matrix::rowSums(raw_mat) > 0,]
+print(paste("Stats out:", dim(raw_mat), "input", c("genes","cells"),"of", name))
+colnames(raw_mat) <- paste(colnames(raw_mat), "1", sep="-")
 
-harmony_redim@cell.embeddings <- harmony_redim@cell.embeddings[match(colnames(liver.integrated), rownames(harmony_redim@cell.embeddings)),]
-liver.integrated@reductions$harmony <- harmony_redim;
-
-
-meta.data <- meta.data[match(colnames(liver.integrated), rownames(meta.data)),]
-liver.integrated@meta.data <- meta.data
-
-
-liver.integrated <- liver.integrated[,liver.integrated@meta.data$Fine_clusters %in% c(8,22)];
-
-# require all genes to be detected in at least one cell in a majority of donors
-gene_filter <- group_rowmeans(liver.integrated@assays$RNA@counts, liver.integrated@meta.data$donor);
-gene_filter <- apply(gene_filter, 1, median)
-gene_filter <- gene_filter > 0;
-
-liver.integrated <- liver.integrated[gene_filter,]
-
-set.seed(7742)
-
-# Dimensionality Reduction
-### Probably need to re-run harmony on just these cell for best results....####
-liver.integrated <- NormalizeData(liver.integrated)
-liver.integrated <- ScaleData(liver.integrated);
-liver.integrated <- FindVariableFeatures(liver.integrated, selection.method = "vst", nfeatures = 2500)
-liver.integrated <- RunPCA(liver.integrated, features = VariableFeatures(object = liver.integrated))
-ElbowPlot(liver.integrated)
+# Get total UMIs that correspond to the min & max thresholds.
+br.out <- barcodeRanks(raw_mat)
+umi_per_barcode <- Matrix::colSums(raw_mat)
 
 
 
-require("harmony")
-set.seed(10131)
-liver.integrated <- RunHarmony(liver.integrated, "donor", plot_convergence = TRUE)
+# get lower threshold for cells
+plausible_cell_threshold <- max(br.out$total[br.out$rank > MAX_CELLS]);
+t_cell_threshold <- quantile(umi_per_barcode[colnames(raw_mat) %in% tcrs[,1]], 0.25)
+b_cell_threshold <- quantile(umi_per_barcode[colnames(raw_mat) %in% bcrs[,1]], 0.25)
+lower_threshold = min(plausible_cell_threshold, t_cell_threshold, b_cell_threshold)
+# get higher threshold for cells
+mandatory_cell_threshold <- max(br.out$total[br.out$rank < MIN_CELLS]);
+t_cell_threshold <- min(umi_per_barcode[colnames(raw_mat) %in% tcrs[tcrs$high_confidence=="True" & tcrs$is_cell == "True",1]])
+b_cell_threshold <- min(umi_per_barcode[colnames(raw_mat) %in% bcrs[bcrs$high_confidence=="True" & bcrs$is_cell=="True",1]])
+higher_threshold = min(mandatory_cell_threshold)
 
+# Run EmptyDrops to try to clean up while allowing more permissive thershold to keep tcrs and bcrs
 
+set.seed(100)
+# Run EmptyDrops
+e.out <- emptyDrops(raw_mat, lower=lower_threshold, niters=100000, ignore=3, retain=higher_threshold)
 
-npcs <- 15
+# Clean up results
+e.out <- e.out[!is.na(e.out$PValue),] # Remove NAs (too few UMIs to estimate PValue)
+e.out$q.value <- e.out$PValue;
+e.out$q.value[e.out$Limited] <- 0; # Set those with p < 1/n_simulations to p = 0 to ensure they are kept after multiple testing correction
+e.out$q.value <- p.adjust(e.out$q.value, method="fdr"); # apply FDR multiple testing correction.
 
-# Cluster with many different parameters
-res <- seq(from=0.1, to=1.5, by=0.2)
-nkNN <- seq(from=30, to=60, by=10)
+keep_cells <- rownames(e.out)[e.out$q.value < 0.01]
+filt_mat <- raw_mat[,match(keep_cells, colnames(raw_mat))]
+tcr_meta <- tcrs[tcrs$barcode %in% keep_cells,]
+bcr_meta <- bcrs[bcrs$barcode %in% keep_cells,]
 
-for(res_param in res) {
-for(nkNN_param in nkNN){
-liver.integrated <- FindNeighbors(liver.integrated, reduction="harmony", dims = 1:npcs, k.param=nkNN_param)
-liver.integrated <- FindClusters(liver.integrated, reduction="harmony", resolution = res_param, k.param=nkNN_param)
-name <- paste("knn_",nkNN_param,"_res_", res_param, sep="");
+# Can't turn tcrs into a metadata table b/c more than one TCR/cell...
 
-liver.integrated@meta.data[[name]] <- liver.integrated@meta.data$seurat_clusters;
+saveRDS(list(mat=filt_mat, tcr=tcr_meta, bcr=bcr_meta), file=paste(name,"TCR_BCR_EmptyDrops.rds", sep="_"))
 
-}}
-
-# Compare all these clusterings
-require(igraph)
-require(gplots)
-clust_table <- liver.integrated@meta.data[, grepl("^knn_", names(liver.integrated@meta.data))]
-clust_table <- as.matrix(apply(clust_table,2,as.numeric))
-require("proxy")
-clust_dists <- proxy::dist(clust_table, method=function(x,y){igraph::compare(x,y,method="vi")}, by_rows=FALSE)
-clust_similr1 <- proxy::simil(clust_table, method=function(x,y){igraph::compare(x,y,method="nmi")}, by_rows=FALSE)
-clust_similr2 <- proxy::simil(clust_table, method=function(x,y){igraph::compare(x,y,method="adjusted.rand")}, by_rows=FALSE)
-
-
-# Find robust exemplar clustering(s)
-require("apcluster")
-require("gplots")
-set.seed(18371)
-
-res1 <- apcluster(-1*as.matrix(clust_dists), p=-5)
-#res2 <- apcluster(as.matrix(clust_similr1), p=-2)
-#res3 <- apcluster(as.matrix(clust_similr1), p=-2)
-
-#valid_clusterings <- res1@exemplars[which(res1@exemplars %in% res2@exemplars & res1@exemplars %in% res3@exemplars)]
-
-coarse_lvl <- "knn_50_res_0.9" # <- Update
-fine_lvl <- "knn_70_res_1.1" # <- Update
-
-#manually select which exemplar to use
-liver.integrated@meta.data$Coarse_clusters <- liver.integrated@meta.data[[coarse_lvl]] 
-liver.integrated@meta.data$Fine_clusters <- liver.integrated@meta.data[[fine_lvl]]
-
-apcluster::heatmap(res1, -1*as.matrix(clust_dists))
-
-png(paste(proj_name, "compare_clusterings_heatmap.png", sep="_"), width=6, height=6, units="in", res=300)
-lab <- matrix("", ncol=ncol(clust_table), nrow=ncol(clust_table))
-lab[colnames(clust_table)==fine_lvl, colnames(clust_table)==fine_lvl] <- "F"
-lab[colnames(clust_table)==coarse_lvl, colnames(clust_table)==coarse_lvl] <- "C"
-heatmap.2(as.matrix(clust_dists), trace="none", distfun=function(x){return(as.dist(clust_dists))}, cellnote=lab)
-dev.off()
-
-
-# Visualize the Chosen clusterings
-nkNN <- 50
-
-set.seed(07219)
-
-liver.integrated <- RunTSNE(liver.integrated, reduction="harmony",  dims = 1:npcs)
-liver.integrated <- RunUMAP(liver.integrated, reduction="harmony",  dims = 1:npcs, parallel=FALSE, n.neighbour=nkNN)
-png(paste(proj_name, "coarse_umap.png", sep="_"), width=6, height=6, units="in", res=50)
-DimPlot(liver.integrated, reduction = "umap", group.by="Coarse_clusters")
-dev.off()
-png(paste(proj_name, "coarse_tsne.png", sep="_"), width=6, height=6, units="in", res=50)
-DimPlot(liver.integrated, reduction = "tsne", group.by="Coarse_clusters")
-dev.off()
-png(paste(proj_name, "coarse_umap_donor.png", sep="_"), width=6, height=6, units="in", res=50)
-DimPlot(liver.integrated, reduction = "umap", group.by="orig.ident")
-dev.off()
-DimPlot(liver.integrated, reduction = "tsne", group.by="orig.ident")
-
-barplot(table(liver.integrated@meta.data$orig.ident, liver.integrated@meta.data$Coarse_clusters), col=rainbow(20))
-
-tab <- table(liver.integrated@meta.data$orig.ident, liver.integrated@meta.data$Coarse_clusters)
-
-DimPlot(liver.integrated, reduction = "umap", group.by="scmap_anno")
-DimPlot(liver.integrated, reduction = "tsne", group.by="scmap_anno")
-png(paste(proj_name, "coarse_umap_autoanno.png", sep="_"), width=6, height=6, units="in", res=50)
-DimPlot(liver.integrated, reduction = "umap", group.by="scmap_anno2")
-dev.off()
-DimPlot(liver.integrated, reduction = "tsne", group.by="scmap_anno2")
-
-saveRDS(liver.integrated, paste(proj_name, "_analysis.rds", sep=""))
 
